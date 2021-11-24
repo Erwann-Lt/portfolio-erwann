2772b2396654b4a89f295d2e75dda68883d5d06a
Erwann Labrot <65678624+Erwann-Lt@users.noreply.github.com>
2021-11-24 16:14:11 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar =()=>{
           <a href="#about" className="ml-3 text-xl">
             Erwann Labrot
           </a>
-        </span>
+        </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#projects" className="mr-5 hover:text-white">
             Projets
