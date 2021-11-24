bdb8fac545913009dcf4ecf15fd3e37ff88d4cb0
Erwann Labrot <65678624+Erwann-Lt@users.noreply.github.com>
2021-11-24 16:07:54 +0100

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -52,9 +52,9 @@ const Contact =()=>{
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <span className="text-indigo-400 leading-relaxed">
+              <a className="text-indigo-400 leading-relaxed" href=''>
                 labrot.erwann@gmail.com
-              </span>
+              </a>
            
             </div>
           </div>
