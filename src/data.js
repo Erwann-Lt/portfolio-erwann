8c715684500d4803ab5b96093fb34205c39a6b14
Erwann-Lt <johndoe@example.com>
2021-11-24 15:50:09 +0100

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,78 @@
+export const projects = [
+    {
+      title: "PROTG",
+      subtitle: "MERN Stack",
+      description:
+        "Front: React + Hooks, Back en cours: Mongo + Express + Node ",
+      image: "https://images.unsplash.com/photo-1567954970774-58d6aa6c50dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29uc3RydWN0aW9uJTIwd29ya3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      link: "https://protg.netlify.app/",
+    },
+    {
+      title: "La boulangerie d'Ymare",
+      subtitle: "React Hooks",
+      description:
+        "Front réalisé avec React, reproduction du site original, responsive: en cours",
+      image: "https://images.unsplash.com/photo-1608198093002-ad4e005484ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJha2VyeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      link: "https://repro-ymare-2021.netlify.app/",
+    },
+    {
+      title: "Enalia",
+      subtitle: "React and Spring",
+      description:
+        "Retranscription du site original en React, responsive: en cours",
+      image: "https://uploads-ssl.webflow.com/5f0c8ee786502d713c4bbd11/5f48b4bc3f2c3d0b9ebba36e_Enalia%20picto.svg",
+      link: "https://repro-enalia-2021.netlify.app/",
+    },
+    {
+      title: "BEAR Coaching",
+      subtitle: "WordPress + JavaScript",
+      description:
+        "WordPress",
+      image: "http://www.bearcoaching.com/wp-content/uploads/2015/03/logo.png",
+      link: "http://www.bearcoaching.com/",
+    },
+    {
+      title: "IFTL",
+      subtitle: "WordPress + JavaScript",
+      description:
+        "WordPress",
+      image: "https://image.freepik.com/free-photo/aerial-view-container-cargo-ship-sea_335224-735.jpg",
+      link: "https://iftl.fr/",
+    },
+  ];
+
+  export const skills=[
+      
+          "React JS","CSS","React-Router",
+          "JavaScript","BootStrap", "Spring",
+          'Node','Express'
+      
+  ];
+
+
+  export const testimonials=[
+    {
+        quote:'Ce fut un réel plaisir de travailler avec Erwann! Il est proactif et a une incroyable soif de découvrir toujours plus ! ',
+        image:'https://tse2.mm.bing.net/th?id=OIP.xXNJ0d6OGDTD2Touj572FQHaJa&pid=Api',
+        name:'Abdullah ',
+        company:'Adobe'
+    },
+     {
+        quote:'Erwann est très professionnel. Il analyse et comprend les besoins très rapidement, ce qui lui permet procure une grande facilité dans son avancement.',
+        image:'https://image.freepik.com/free-vector/hand-drawn-ethnic-wolf-pack_23-2147646758.jpg',
+        name:'Damien',
+        company:'The Wolf'
+    },
+    {
+        quote:'Erwann a un personnalité très agréable. Il aime apprendre et trouve toujours comment repousser ses limites.',
+        image:'https://tse4.mm.bing.net/th?id=OIP.qNjdZ7moskcnAync-Y4FDgHaD5&pid=Api',
+        name:'Cyril',
+        company:'Doctolib'
+    },
+    {
+        quote:'Erwann a su comprendre et répondre très rapidement aux besoins de mon site, ce fut un grand plaisir de travailler avec lui.',
+        image:'http://www.bearcoaching.com/wp-content/uploads/2015/03/logo.png',
+        name:'Reda',
+        company:'BEAR Coaching'
+    }
+];
