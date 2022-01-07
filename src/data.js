e8de2b358d2529003934d19271c8596f656a8a49
Erwann Labrot <65678624+Erwann-Lt@users.noreply.github.com>
2022-01-07 14:45:32 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,4 +1,12 @@
 export const projects = [
+     {
+      title: "Clone Instagram",
+      subtitle: "Full stack",
+      description:
+        "Front: React + Hooks + Tailwind, Back: Firebase (Firestore/auth)",
+      image: "https://images.unsplash.com/photo-1611162616305-c69b3fa7fbe0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aW5zdGFncmFtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      link: "https://erstagram.netlify.app/",
+    },
     {
       title: "PROTG",
       subtitle: "MERN Stack",
