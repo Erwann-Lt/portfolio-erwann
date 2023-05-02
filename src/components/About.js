253452f5e17fbf08167052ccda6d639d6f5343e4
Erwann Labrot <65678624+Erwann-Lt@users.noreply.github.com>
2023-05-02 11:06:49 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,15 @@
 const About =()=>{
     return(
         <section id="about">
+            <div class="relative flex items-center 
+        justify-center h-screen overflow-hidden">
+        <video src=
+        "https://assets.mixkit.co/videos/preview/mixkit-happy-man-running-at-low-tide-21706-large.mp4"
+            autoplay="{true}" loop muted
+            className="absolute z-10 w-auto 
+            min-w-full min-h-full max-w-none">
+        </video>
+        </div>
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
