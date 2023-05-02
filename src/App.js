d5b7b42ce621b430b682f64a1bcbe9fc268a0f32
Erwann Labrot <65678624+Erwann-Lt@users.noreply.github.com>
2023-05-02 11:03:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,25 @@ import Contact from './components/Contact'
 
 const App=()=> {
   return (
-    <main className="text-gray-400 bg-gray-900 body-font">   
+    <main className="text-gray-400 bg-gray-900 body-font">  
+    <div style="text-align: center bg-black">
+        <h1 class="text-green-600 
+                text-3xl font-bold">
+            GeeksforGeeks
+        </h1>
+        <h3 class="text-xl text-white">
+            Bienvenue
+        </h3>
+    </div>
+    <div class="relative flex items-center 
+        justify-center h-screen overflow-hidden">
+        <video src=
+        "https://assets.mixkit.co/videos/preview/mixkit-happy-man-running-at-low-tide-21706-large.mp4"
+            autoplay="{true}" loop muted
+            className="absolute z-10 w-auto 
+            min-w-full min-h-full max-w-none">
+        </video>
+    </div>
       <Navbar />
       <About />
       <Projects />
