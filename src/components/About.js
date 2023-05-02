f764d5b456e47c20e11e8186e8253edbf58a203d
Erwann Labrot <65678624+Erwann-Lt@users.noreply.github.com>
2023-05-02 11:13:15 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About =()=>{
             <div class="relative flex items-center 
         justify-center h-screen overflow-hidden">
         <div class="z-50 flex items-center 
-        justify-center h-screen">
+        justify-center h-screen text-2xl text-black">
         <h1> La joie de vivre</h1>
         <h2>La joie du code</h2>
         </div>
